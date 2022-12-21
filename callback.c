16c3136d63a0cbc48533bdd1e5b637d7efacbfa9
SANAGO <sanago3130@gmail.com>
2022-12-21 01:08:24 +0100

    affichage des informations, projet fini.

--- callback.c
+++ callback.c
@@ -10,10 +10,27 @@ size_t callback(void *ptr, size_t size, size_t nmemb, void *stream)
 	json_object *root = json_tokener_parse(data);
 
 	json_object *main_obj = json_object_object_get(root, "main");
+	json_object *wind_obj = json_object_object_get(root, "wind");
+	json_object *wind_speed_obj = json_object_object_get(wind_obj, "speed");
 	json_object *temp_obj = json_object_object_get(main_obj, "temp");
-	double temp = json_object_get_double(temp_obj);
+	json_object *temp_min_obj = json_object_object_get(main_obj, "temp_min");
+	json_object *temp_max_obj = json_object_object_get(main_obj, "temp_max");
+	json_object *humidity_obj = json_object_object_get(main_obj, "humidity");
+	json_object *name_obj = json_object_object_get(root, "name");
 
-	printf("Temperature : %f\n", temp);
+	double temp = json_object_get_double(temp_obj);
+	double temp_min = json_object_get_double(temp_min_obj);
+	double temp_max = json_object_get_double(temp_max_obj);
+	double humidity = json_object_get_double(humidity_obj);
+	char *name = json_object_get_string(name_obj);
+	double wind_speed = json_object_get_double(wind_speed_obj);
+
+	printf("Ville : %s\n", name);
+	printf("Temperature : %f'C\n", temp);
+	printf("Temperature maximale : %f'C\n", temp_max);
+	printf("Temperature minimale : %f'C\n", temp_min);
+	printf("Humidite : %f%%\n", humidity);
+	printf("Vitesse du vent : %fkm/h\n", wind_speed * 3.6);
 
 	json_object_put(root);
 
